43216bfcc5db6c895586c5d0d960017fe449691e
mrdatafreak <105959085+mrdatafreak@users.noreply.github.com>
2022-12-30 15:39:05 +0000

    adding the test

--- hello.py
+++ hello.py
@@ -1,24 +1,22 @@
 import click
 import glob
 
+
 @click.command()
 @click.option(
     "--path",
-    prompt = "Path to search csv files",
-    help = "this is the path to search for files: tmp",
+    prompt="Path to search csv files",
+    help="this is the path to search for files: tmp",
 )
-
 @click.option(
-    "--ftype", 
-    prompt = "Pass in the type of file",
-    help = "Pass in the file type: i.e csv"
-    )
-
+    "--ftype", prompt="Pass in the type of file", help="Pass in the file type: i.e csv"
+)
 def search(path, ftype):
-    results = glob.glob(f"{path}/and*.{ftype}")
-    click.echo(click.style("Found Matches:", fg='red'))
+    """This is a tool that search for patters line *.csv"""
+    results = glob.glob(f"{path}/*.{ftype}")
+    click.echo(click.style("Found Matches:", fg="red"))
     for result in results:
-        click.echo(click.style(f"{result}", fg='white', bg='blue'))
+        click.echo(click.style(f"{result}", fg="white", bg="yellow"))
 
 
 if __name__ == "__main__":
